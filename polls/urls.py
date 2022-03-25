cddca5d2445da32aa1e392513d3e9bbf77a5de2f
riser <riserlarenss@gmail.com>
2022-03-25 23:26:16 +0300

    ok

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_i>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     path('owner', views.owner, name='owner'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
